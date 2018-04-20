d184cb758bbf4f39044d331ed1a5884d056eb1d6
Sara Hamilton <sara@indeterminate.net>
2018-04-20 14:55:32 -0700

    add model for reviews

--- GummiBearKingdom/Models/Review.cs
+++ GummiBearKingdom/Models/Review.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace GummiBearKingdom.Models
 {
+    [Table("Reviews")]
     public class Review
     {
         public Review()
         {
+            this.Reviews = new HashSet<Review>();
         }
+        [Key]
+        public int ReviewId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Rating { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
     }
 }
